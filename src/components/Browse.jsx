81aab5232c834f8cdb1d39012d11121744d49e0a
surya <varma.surya@protonmail.com>
2024-08-18 20:27:31 +0530

    chore: Add moviesSlice and dispatch nowPlayingMovies in Browse component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
 import { API_OPTIONS, TMDB_NOW_PLAYING } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/redux/moviesSlice";
+
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(TMDB_NOW_PLAYING, API_OPTIONS);
     const json = await data.json();
     console.log(json);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
